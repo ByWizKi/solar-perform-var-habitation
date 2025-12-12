81211568c9a0c0b50a8c5a4bb879e161b052ec08
THIEBAUD Enzo <enzoth39260@gmail.com>
2025-12-12 14:22:07 +0100

    Merge branch 'develop' into main

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -180,7 +180,7 @@ export default function Navigation() {
                       d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                     />
                   </svg>
-                  <span className="hidden xl:inline">Dconnexion</span>
+                  <span className="hidden xl:inline">Déconnexion</span>
                 </button>
               </>
             )}
@@ -272,7 +272,7 @@ export default function Navigation() {
                     d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
                   />
                 </svg>
-                Dconnexion
+                Déconnexion
               </button>
             )}
           </div>
